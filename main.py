dfc5143e4b83359cafdc5182068c2ba93f658a2e
thomashiemstra <thomas-hiemstra@hotmail.com>
2017-12-28 22:44:26 +0100

    collission checking seems to work now

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
+from os import environ
+environ['MPLBACKEND'] = 'module://gr.matplotlib.backend_gr'
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-from itertools import product, combinations
-from numpy import sin, cos, pi
+from numpy import sin, cos, pi, sqrt
 
+#fig = plt.figure()
+#ax = Axes3D(fig)
+#ax.set_xlim(-10, 10)
+#ax.set_ylim(0, 20)
+#ax.set_zlim(0, 20)
+#ax.set_xlabel('X ---->')
+#ax.set_ylabel('Y ---->')
+#ax.set_zlabel('Z ---->')
 
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.set_xlim(-10, 10)
-ax.set_ylim(0, 20)
-ax.set_zlim(0, 20)
-ax.set_xlabel('X ---->')
-ax.set_ylabel('Y ---->')
-ax.set_zlabel('Z ---->')
-
-class cube:
+class box:
+    size = np.zeros(3)
+    pos = np.zeros(3)
     
-    size = np.array(3)
-    pos = np.array(3)
-    
-    
-    def __init__(self, size, pos, angle, radius=0.0):
+    def __init__(self, size, pos, angle):
         self.size = size
         self.pos = pos
         self.angle = angle
-        self.radius = radius
 
     def cuboid_data(self):
         # code taken from
@@ -66,35 +64,57 @@ class cube:
 #    vector of the point expressed in the cube's coordinate frame
     def getRPrime(self, r_point):
         c, s = np.cos(self.angle), np.sin(self.angle)
-        R = np.matrix([[c, -s], [s, c]])
+        R = np.array([[c, -s, 0], [s, c, 0],[0, 0, 1]])
         
-        r_temp = np.array(3)
+        r_temp = np.zeros(3)
         for i in range(0,3):
             r_temp[i] = r_point[i] - self.pos[i]
         
-        return R.dot(r_temp)
+        R = R.transpose()
+                
         
-    def checkColission(self, r_point):
-        x,y,z = self.getRPrime(r_point)
+        return np.asarray(R.dot(r_temp))
+    
+        
+    def checkColission(self, r_point, radius=0.0):
+        r_prime = self.getRPrime(r_point)
+        
+        x = r_prime[0]
+        y = r_prime[1]
+        z = r_prime[2]
+        
+        
+        print(x,y,z)
         L, W, H = self.size
         
-        
         if x < 0:
-            dx = np.maximum(-x + self.radius, 0) #dx > 0 means collision due to radius of point
+            dx = np.maximum(-x + radius, 0) #dx > 0 means collision due to radius of point
         elif x > L:
-            dx = np.minimum(L - x + self.radius,0) #dx < 0 means collision due to radius of point
+            dx = np.minimum(L - x + radius,0) #dx < 0 means collision due to radius of point
         else:
             dx = 0
+            
+        if y < 0:
+            dy = np.maximum(-y + radius, 0) #dx > 0 means collision due to radius of point
+        elif y > W:
+            dy = np.minimum(L - y + radius,0) #dx < 0 means collision due to radius of point
+        else:
+            dy = 0
+            
+        if z < 0:
+            dz = np.maximum(-z + radius, 0) #dx > 0 means collision due to radius of point
+        elif z > H:
+            dz = np.minimum(L - z + radius,0) #dx < 0 means collision due to radius of point
+        else:
+            dz = 0
         
-        #TODO Y AND Z COPY PASYE
+        r = sqrt(dx*dx + dy*dy + dz*dz)
         
-        return dx
+        return r
+
+
         
-        
-        
-        
-        
-        
-        
-cube1 = cube([5,5,5], [0,0,0], -pi/4)
-cube1.plot(ax)
+cube = box([5,5,5], [5,5,0], 0)
+r = cube.checkColission([7,6,0])
+print(r)
+#cube.plot(ax)
